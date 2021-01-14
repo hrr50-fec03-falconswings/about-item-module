69b4d812d49143a58f0bfa06fa1c4ebe5dcad469
kyleperra <perra.kyle@gmail.com>
2021-01-13 19:37:37 -0800

    removed categories table, simplified DB structure

--- server/schema.sql
+++ server/schema.sql
@@ -2,20 +2,15 @@ drop database if exists products_db;
 create database products_db;
 use products_db;
 
-create table product_categories (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL UNIQUE,
-  PRIMARY KEY (id)
-);
-
 create table products (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   images TEXT NOT NULL,
   details TEXT NOT NULL,
   specifications TEXT NOT NULL,
-  product_category INT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (product_category) REFERENCES product_categories(id)
+  product_category VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 
+-- insert electronics
+insert into products (name, images, details, specifications, product_category) values ('computer', 'an image', 'some details', 'the specs', 'electronics');
