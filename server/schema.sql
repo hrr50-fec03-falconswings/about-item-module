5d41904eb1ff1925055709f7b95b3ffa7973e539
kyleperra <perra.kyle@gmail.com>
2021-01-13 18:22:40 -0800

    outlined DB schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,3 +1,21 @@
-drop database if exists about_item;
-create database about_item;
-use about_item;
+drop database if exists products_db;
+create database products_db;
+use products_db;
+
+create table product_categories (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL UNIQUE,
+  PRIMARY KEY (id)
+);
+
+create table products (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  images TEXT NOT NULL,
+  details TEXT NOT NULL,
+  specifications TEXT NOT NULL,
+  product_category INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (product_category) REFERENCES product_categories(id)
+);
+
