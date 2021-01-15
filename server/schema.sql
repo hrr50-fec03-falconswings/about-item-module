b177ae0b3a819dc1668c6280f7714c2f07b9a4b3
kyleperra <perra.kyle@gmail.com>
2021-01-14 18:26:16 -0800

    refactored db schema to use one products table

--- server/schema.sql
+++ server/schema.sql
@@ -13,12 +13,12 @@ create table products (
 );
 
 -- insert electronics
-insert into products (name, images, details, specifications, product_category) values ('computer', 'an image', 'some details', 'the specs', 'electronics');
+-- insert into products (name, images, details, specifications, product_category) values ('computer', 'an image', 'some details', 'the specs', 'electronics');
 
-insert into products (name, images, details, specifications, product_category) values ('computer2', 'an image', 'some details', 'the specs', 'electronics');
+-- insert into products (name, images, details, specifications, product_category) values ('computer2', 'an image', 'some details', 'the specs', 'electronics');
 
-insert into products (name, images, details, specifications, product_category) values ('Dynasty Warriors 1', 'an image 1', 'some details', 'the specs', 'games');
+-- insert into products (name, images, details, specifications, product_category) values ('Dynasty Warriors 1', 'an image 1', 'some details', 'the specs', 'games');
 
-insert into products (name, images, details, specifications, product_category) values ('Dynasty Warriors 2', 'an image 2', 'some details', 'the specs', 'games');
+-- insert into products (name, images, details, specifications, product_category) values ('Dynasty Warriors 2', 'an image 2', 'some details', 'the specs', 'games');
 
-insert into products (name, images, details, specifications, product_category) values ('Dynasty Warriors 3', 'an image 3', 'some details', 'the specs', 'games');
+-- insert into products (name, images, details, specifications, product_category) values ('Dynasty Warriors 3', 'an image 3', 'some details', 'the specs', 'games');
