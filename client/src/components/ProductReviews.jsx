3aa27ed8c84784e2a665cfdb67691c2ed05c9cc0
kyleperra <perra.kyle@gmail.com>
2021-01-20 15:26:49 -0800

    adds the correct number of full, half and empty stars based off product reviews_avg

--- client/src/components/ProductReviews.jsx
+++ client/src/components/ProductReviews.jsx
@@ -6,24 +6,38 @@ import halfStar from '../../../public/assets/icons/half_star.svg';
 import fullStar from '../../../public/assets/icons/full_star.svg';
 
 const ProductReviews = ({ product }) => {
-  let totalStars = [];
-  let reviews = product.reviews_avg;
-  for (let i = Math.floor(reviews); i > 0; i--) {
-    totalStars.push(1);
-  }
-  if (reviews % 2 !== 0) totalStars.push(.5);
-  while (totalStars.length < 5) {
-    totalStars.push(0);
+  const initStars = () => {
+    let totalStars = [];
+    let reviews = product.reviews_avg;
+    for (let i = Math.floor(reviews); i > 0; i--) {
+      totalStars.push(1);
+    }
+    // if (1 - Math.abs(reviews % 2) < .1) console.log(reviews);
+    if (reviews % 2 !== 0) totalStars.push(.5);
+    while (totalStars.length < 5) {
+      totalStars.push(0);
+    }
+
+    return totalStars;
   }
 
-  console.log(totalStars);
   return (
     <div className="tile-content-reviews">
-      <img src={fullStar} alt="" style={{height: '11px'}} />
-      <img src={fullStar} alt="" style={{height: '11px'}} />
-      <img src={fullStar} alt="" style={{height: '11px'}} />
-      <img src={halfStar} alt="" style={{height: '11px'}} />
-      <img src={emptyStar} alt="" style={{height: '11px'}} />
+      {initStars().map((star, index) => {
+        if (star === 1) {
+          return (
+            <img src={fullStar} alt="a full star" style={{height: '11px'}} key={index} />
+          )
+        } else if (star === .5) {
+          return (
+            <img src={halfStar} alt="a half-full star" style={{height: '11px'}} key={index} />
+          )
+        } else {
+          return (
+            <img src={emptyStar} alt="an empty star" style={{height: '11px'}} key={index} />
+          )
+        }
+      })}
     </div>
   );
 };
